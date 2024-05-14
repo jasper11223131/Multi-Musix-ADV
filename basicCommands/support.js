767c27caef27ca3b45e414aa8af6969ae30412c4
jasper11223131 <163884717+jasper11223131@users.noreply.github.com>
2024-05-14 11:05:04 +0600

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,10 +4,10 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+        const supportServerLink = "https://discord.gg/";
+      const githubLink = "https://github.com/Madara-2";
+      const replitLink = "https://replit.com/@abdurrahmanjona";
+      const instagramLink = "https://www.instagram.com/jasper_skye_0/";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -15,8 +15,8 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- Instagram - ${instagramLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1239564192114020384/1239803787917393992/standard_6.gif?ex=664440c1&is=6642ef41&hm=4c00f753773a775585d655f3600532d44ec807d09b286b7a0553088d2712def8&')
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
